6ae226341fa78c39e7c244558fa97a0737eca8ff
Denis Gruzdev <codingjerk@gmail.com>
2023-12-28 13:21:04 +0000

    fix: build new image first and restart after on deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 set -Eeuo pipefail
 
+docker build --tag cj.dog .
+
 docker stop cj.dog || true
 docker rm cj.dog || true
 
-docker build --tag cj.dog .
 docker run --detach --publish=80:80 --name=cj.dog --restart=always cj.dog
